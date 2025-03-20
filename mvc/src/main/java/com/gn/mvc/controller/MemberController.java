7144821aee862296d202446bd89106cf9eb52b7c
sbsophie <devsophie37@gmail.com>
2025-03-20 16:58:43 +0900

    [psb] 회원수정,탈퇴

--- mvc/src/main/java/com/gn/mvc/controller/MemberController.java
+++ mvc/src/main/java/com/gn/mvc/controller/MemberController.java
@@ -5,14 +5,19 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.entity.Member;
 import com.gn.mvc.service.MemberService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,6 +25,59 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 
 	private final MemberService service;
+	
+	//회원탈퇴 기능 코드
+	@DeleteMapping("/member/{id}")
+	@ResponseBody
+	public Map<String,String> memberDeleteApi(@PathVariable("id") Long id,
+			HttpServletResponse response){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원 탈퇴중 오류가 발생하였습니다.");
+		
+		int result = service.deleteMember(id);
+		if(result > 0) {
+			Cookie rememberMe = new Cookie("remember-me",null);
+			rememberMe.setMaxAge(0);
+			rememberMe.setPath("/");
+			response.addCookie(rememberMe);
+			
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "정상적으로 회원탈퇴가 되었습니다.");
+		}
+		return resultMap;
+	}
+	
+	//회원수정 기능 코드
+	@PostMapping("/member/{id}/update")
+	@ResponseBody
+	public Map<String,String> memberUpdateApi(MemberDto param,
+			HttpServletResponse response){
+		Map<String,String> resultMap = new HashMap<String,String>();
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원 수정중 오류가 발생하였습니다.");
+		
+		int result = service.updateMember(param);
+		if(result > 0){
+			// 쿠키(remember-me) 무효화 코드
+			Cookie rememberMe = new Cookie("remember-me",null);
+			rememberMe.setMaxAge(0);
+			rememberMe.setPath("/");
+			response.addCookie(rememberMe);
+			
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "회원정보가 수정되었습니다.");
+		}
+		return resultMap;
+	}
+	
+	//회원수정페이지로 이동하는 코드
+	@GetMapping("/member/{id}/update")
+	public String memberUpdateView(@PathVariable("id") Long id, Model model) {
+		Member member = service.selectMemberOne(id);
+		model.addAttribute("member",member);
+		return "member/update";
+	}
 	
 	// 로그인페이지로 이동하는 코드 (+MyLoginFailureHandler코드가 넘어오는 곳)
 	@GetMapping("/login")
--- mvc/src/main/java/com/gn/mvc/service/MemberService.java
+++ mvc/src/main/java/com/gn/mvc/service/MemberService.java
@@ -1,9 +1,18 @@
 package com.gn.mvc.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.entity.Board;
 import com.gn.mvc.entity.Member;
 import com.gn.mvc.repository.MemberRepository;
 
@@ -15,6 +24,61 @@ public class MemberService {
 	
 	private final MemberRepository repository;
 	private final PasswordEncoder passwordEncoder;	// 암호화로직을 추가
+	private final DataSource dataSource;
+	private final UserDetailsService userDetailsService;
+
+	// 회원탈퇴
+	public int deleteMember(Long id) {
+		int result = 0;
+		try {
+			Member target = repository.findById(id).orElse(null);
+			if (target != null) {
+				repository.deleteById(id);
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM persistent_logins WHERE username= ?";
+				jdbcTemplate.update(sql,target.getMemberId());
+				SecurityContextHolder.getContext().setAuthentication(null);
+				result = 1;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+		
+	// 회원수정 기능 코드
+	public int updateMember(MemberDto param) {
+		int result = 0;
+		try{
+			//역할 
+			//(1)데이터베이스 회원정보수정 
+			param.setMember_pw(passwordEncoder.encode(param.getMember_pw()));
+			Member updated = repository.save(param.toEntity());
+			if(updated != null) {
+				//(2)remember-me(DB,cookie)가 있다면 무효화 
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM persistent_logins WHERE username= ?";
+				jdbcTemplate.update(sql,param.getMember_id());
+				//(3)변경된 회원 정보 Security Context에 즉시반영
+				UserDetails updatedUserDetails
+					= userDetailsService.loadUserByUsername(param.getMember_id());
+				Authentication newAuth = new UsernamePasswordAuthenticationToken(
+						updatedUserDetails,
+						updatedUserDetails.getPassword(),
+						updatedUserDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(newAuth);
+				result = 1;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	//회원수정 하기위해 데이터를 넘겨주는 코드(PK값 기준 회원 정보 단일 조회 후 반환하는 역할)
+	public Member selectMemberOne(Long id) {
+		return repository.findById(id).orElse(null);
+	}
 	
 	public MemberDto createMember(MemberDto dto) {
 //		String oriPw = dto.getMember_pw();
@@ -26,4 +90,6 @@ public class MemberService {
 		Member saved = repository.save(entity);
 		return new MemberDto().toDto(saved);
 	}
+
+
 }
