9ea1fff00ab867919eb1386d987e74422a8b1af4
sbsophie <devsophie37@gmail.com>
2025-03-28 16:54:06 +0900

    [psb] todo_attach code commit

--- todo/src/main/java/com/gn/todo/TodoApplication.java
+++ todo/src/main/java/com/gn/todo/TodoApplication.java
@@ -1,5 +1,6 @@
 package com.gn.todo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,14 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class TodoApplication implements WebMvcConfigurer {
 
+	@Value("${ffupload.location}")
+	private String fileDir;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/uploads/**")
+			.addResourceLocations("file:///"+fileDir);
     }
-
+	
 }
